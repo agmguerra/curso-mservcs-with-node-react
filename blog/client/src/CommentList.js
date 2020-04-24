f266cf76bb9a40175376b5bdf7843542d0a987e5
AlexandreGuerra <agmguerra@gmail.com>
2020-04-23 21:32:44 -0300

    implementação do serviço de query e do event bus para tratar o processo assincrono

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,24 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+import React from 'react'
 
-export default ({ postId }) => {
+export default ({ comments }) => {
 
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const res = await axios.get(`http://localhost:4001/post/${postId}/comments`);
-
-    setComments(res.data);
-  }
-
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line
-  }, []);
-
-  const renderedComments = comments.map(comment => {
+  const renderedComments = comments.length > 0 && comments.map(comment => {
     return <li key={comment.id}>{comment.content}</li>
-  })
+  });
 
   return(
     <ul>
